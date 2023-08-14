92e64b464b583292b6bb112bfb178e91f4872cc3
Arbai <93751741+MrArbai@users.noreply.github.com>
2023-08-14 09:00:14 +0700

    comit

---
+++ semester 2/PBO/TAS223220066/src/main/java/com/mycompany/tas223220066/TAS223220066.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tas223220066;
+
+/**
+ *
+ * @author ITD31
+ */
+public class TAS223220066 {
+
+    public static void main(String[] args) {
+        new DataPasienProgram().setVisible(true);
+    }
+}
---
+++ semester 2/PBO/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import Form.frmLogin;
+
+/**
+ *
+ * @author ITD31
+ */
+public class Mavenproject1 {
+    public static void main(String[] args) {
+        new frmLogin().setVisible(true);
+    }
+}
